cb677e1ee659eec5b5a09441cfb3d1b5b163f812
viktro123 <104514516+viktro123@users.noreply.github.com>
2022-04-27 17:13:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var settingsmenu = document.querySelector(".setings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuТоggle(){
+	settingsmenu.classList.toggle("setings-menu-height");
+
+}
+darkBtn.onclick = function(){
+	darkBtn.classList.toggle("dark-btn-on");
+	document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light"){
+    	localStorage.setItem("theme", "dark");
+ 
+}
+else{
+	localStorage.setItem("theme", "li");
+}
+
+
+
+}
+
+
+
+if(localStorage.getItem("theme") == "light"){
+	darkBtn.classList.remove("dark-btn-on");
+	document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+	darkBtn.classList.add("dark-btn-on");
+	document.body.classList.add("dark-theme");
+}
+else{
+	localStorage.setItem("theme", "light");
+
+}
+
+
+
+
+
+
